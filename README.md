eb8cffd66ada80eb7512062049cc009b0bafaea7
gitadmin <jian.songyj@powerleader.com.cn>
2021-03-24 05:30:25 +0000

    modify README.md, enhance book_start.sh

--- README.md
+++ README.md
@@ -1,43 +1,57 @@
 # docker-gitbook
-#1. 下载本仓库
+
+# 1. 下载本仓库
+```
+cd ~
 
 git clone https://github.com/SJ5J/docker-gitbook.git
+
 cd ./docker-gitbook
+```
 
-#2. SUMMARY.md是我的笔记，要编辑或替换成你自己的，或者直接删掉。
-#3. gitbook_plugin.tar 里是gitbook的配置文件book.json 和对应的插件
-#放这个包的作用是跳过gitbook install 下载插件，加快容器启动
+# 2. SUMMARY.md
+这是我的文档目录，要编辑或替换成你自己的，或者直接删掉。
 
-#4. 构建镜像
+# 3. gitbook_plugin.tar 
+这是gitbook的配置文件和插件目录，放这个包的作用是跳过gitbook install 下载插件，加快容器启动
+- book.json 配置文件
+- node_modlues 对应的插件
 
+# 4. 构建镜像
+```
 docker build ./ -t gitbook:3.2.0
+```
 
-#5. 启动容器 $YOUR_PATH 替换成你的绝对路径
-
+# 5. 启动容器 
+```
 docker run -itd --rm --name gitbook \
 -p 4000:4000 -p 35729:35729 \
--v $YOUR_PATH/docker-gitbook:/book \
+-v $HOME/docker-gitbook:/book \
 gitbook:3.2.0
+```
 
-#6. 浏览器访问
+# 6. 浏览器访问
 
 http://localhost:4000
 
 
-#7. 进入容器(可选)
-
+# 7. 进入容器 (可选)
+```
 docker exec -ti gitbook /bin/bash
+```
 
+# 8. 容器启动脚本 (book_start.sh)
+## 8.1 容器启动时默认动作是启动gitbook服务，即执行
+```
+./book_start.sh serve /book/ /book/docs/
+```
 
-#8. book_start.sh 容器启动脚本用法
-#8.1 容器启动时默认动作是启动gitbook服务，即执行
+## 8.2 如果只需要初始化SUMMARY.md， 即 gitbook init，则
+```
+./book_start.sh init /book/
+```
 
-book_start.sh serve /book/ /book/docs/
-
-#8.2 如果只需要初始化SUMMARY.md， 即 gitbook init，则
-
-book_start.sh init /book/
-
-#8.3 如果只是想更新生成最新的html，则
-
-book_start.sh build /book/ /book/docs/
+## 8.3 如果只是想更新生成最新的html，则
+```
+./book_start.sh build /book/ /book/docs/
+```
--- book_start.sh
+++ book_start.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 #电子书根目录，参考Dockerfile的VOLUME挂载点
 #生成html的目录名必须是docs，以便用github的pages服务托管
-ROOT_PATH=/book
+ROOT_PATH=$PWD
+PLUGIN_PACKAGE=gitbook_plugin.tar.gz
+
 CMD=$1
 MD_PATH=$2
 BOOK_PATH=$3
+
+
+if [ $CMD != "serve" -a $CMD != "init" -a $CMD != "build" ] ||
+   ([ $CMD == "serve" -o  $CMD == "build" ]) && [ $# -ne 3 ] ||
+   [ $CMD == "init" -a $# -ne 2 ];then
+
+    echo "usage:"
+    echo "To start gitbook:   ./book_start.sh serve PATH_TO_MARKDOWN PATH_TO_HTML"
+    echo "To build html book: ./book_start.sh build PATH_TO_MARKDOWN PATH_TO_HTML"
+    echo "To generate SUMMARY.md: ./book_start.sh init PATH_TO_MARKDOWN"
+    exit 0
+fi
 
 # init SUMMARY.md and md file directory
 
 if [ ! -d "$MD_PATH" ]; then
     # MD_PATH 目录不存在
     mkdir -p "$MD_PATH"
-    cp -f $ROOT_PATH/SUMMARY.md $ROOT_PATH/README.md $MD_PATH/
+    cp -f $ROOT_PATH/SUMMARY.md $ROOT_PATH/README.md $ROOT_PATH/introduction.md $MD_PATH/
 elif [ "`ls -A $MD_PATH`" = "" ]; then
     #MD_PATH 目录为空
-    cp -f $ROOT_PATH/SUMMARY.md $ROOT_PATH/README.md $MD_PATH/
+    cp -f $ROOT_PATH/SUMMARY.md $ROOT_PATH/README.md $ROOT_PATH/introduction.md $MD_PATH/
 fi
 
 # 初始化生成空的SUMMARY.md文件和对应目录，格式： gitbook init [生成SUMMARY.md 的路径]
@@ -28,27 +42,22 @@ fi
 if [ ! -d "$BOOK_PATH" ]; then
     #BOOK_PATH 目录不存在
     mkdir -p $BOOK_PATH
-    #如果没网络可以复制下载好的gitbook插件，并跳过下载插件这步 #gitbook install $BOOK_PATH/
-    #cp -f $ROOT_PATH/book.json $BOOK_PATH/
-    tar xf $ROOT_PATH/gitbook_plugin.tar -C $BOOK_PATH/
-    #rm -f $ROOT_PATH/gitbook_plugin.tar
-elif [ "`ls -A $BOOK_PATH`" = "" ]; then
+fi
+
+if [ "`ls -A $BOOK_PATH`" = "" ]; then
     #BOOK_PATH 目录为空
     #如果没网络可以复制下载好的gitbook插件，并跳过下载插件这步 #gitbook install $BOOK_PATH/
     #cp -f $ROOT_PATH/book.json $BOOK_PATH/
-    tar xf $ROOT_PATH/gitbook_plugin.tar -C $BOOK_PATH/
+    tar xzf $ROOT_PATH/$PLUGIN_PACKAGE -C $BOOK_PATH/
     #rm -f $ROOT_PATH/gitbook_plugin.tar
 fi
 
-if [ $CMD == "serve" ]; then
-    #gitbook serve [SUMMARY.md 所在路径] [生成html的目标路径]
-    #nohup gitbook serve $ROOT_PATH/ $ROOT_PATH/html/ 2>&1 &
-    gitbook serve $MD_PATH/ $BOOK_PATH/ 2>&1
-    exit 0
-fi
+#$CMD == "build"
+#如果更新了md文档，可以用这个build命令生成新的html，但是gitbook会自动更新所以用不到
 
-if [ $CMD == "build" ]; then
-    #如果更新了md文档，可以用这个build命令生成新的html，但是gitbook会自动更新所以用不到
-    gitbook build $MD_PATH/ $BOOK_PATH/
-    exit 0
-fi
+#$CMD == "serve" ]; then
+#gitbook serve [SUMMARY.md 所在路径] [生成html的目标路径]
+#nohup gitbook serve $ROOT_PATH/ $ROOT_PATH/html/ 2>&1 &
+gitbook $CMD $MD_PATH/ $BOOK_PATH/ 2>&1
+exit 0
+
